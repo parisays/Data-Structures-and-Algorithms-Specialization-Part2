9b93a2469ad52a3272b2ed143c7485fd9eee03ee
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-04-09 18:10:42 +0430

    Add Q1

--- A6/A6/Q1ConstructBWT.cs
+++ A6/A6/Q1ConstructBWT.cs
@@ -11,15 +11,51 @@ namespace A6
     {
         public Q1ConstructBWT(string testDataName) : base(testDataName)
         {
+            //this.ExcludeTestCaseRangeInclusive(11, 50);
         }
 
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<String, String>)Solve);
 
+
+        private List<string> CyclicRotations { get; set; }
+
+        //private SortedList<string, int> Cycles { get; set; }
+
         public string Solve(string text)
         {
-            // write your code here
-            throw new NotImplementedException();
+            int textSize = text.Length;
+            CyclicRotations = new List<string>();
+            //Cycles = new SortedList<string, int>();
+            for (int i = 0; i < textSize; i++)
+            {
+                StringBuilder newCyclicRotation = new StringBuilder(textSize);
+                int index = i;
+                for (int j = 0; j < textSize; j++)
+                {
+                    newCyclicRotation.Insert(j, text[index]);
+                    
+                    index = (index + 1) % textSize;
+                }
+
+                CyclicRotations.Add(newCyclicRotation.ToString());
+                //Cycles.Add(newCyclicRotation.ToString(), i);
+            }
+
+            return GetBurrowsWheelerTransform(textSize);
+        }
+        
+
+        private string GetBurrowsWheelerTransform(int textSize)
+        {
+            CyclicRotations.Sort();
+            StringBuilder result = new StringBuilder(textSize);
+            for (int i = 0; i < textSize; i++)
+                result.Insert(i, CyclicRotations[i][textSize - 1]);
+            //result.Insert(i, Cycles.ElementAt(i).Key.Last());
+
+
+            return result.ToString();
         }
     }
 }
