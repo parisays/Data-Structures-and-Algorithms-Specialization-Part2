ed6464174c14fc67df08f8dd990b008947540ffa
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-04-13 21:22:52 +0430

    Add Test for Q2

---
+++ A6/A6Tests/Q2ReconstructStringFromBWTTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A6.Tests
+{
+    [TestClass()]
+    public class Q2ReconstructStringFromBWTTests
+    {
+        [TestMethod()]
+        public void Q2ReconstructStringFromBWTTest()
+        {
+            var q2 = new Q2ReconstructStringFromBWT("TD2");
+            Assert.AreEqual("banana$", q2.Solve("annb$aa"));
+        }
+    }
+}
