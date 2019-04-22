d129b9510baf324cbe72745d15e28752b6fa2d7d
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-04-22 19:38:24 +0430

    Add Final Solutions

--- Exam1/Exam1/Q2Cryptanalyst.cs
+++ Exam1/Exam1/Q2Cryptanalyst.cs
@@ -10,9 +10,7 @@ namespace Exam1
     {
         public Q2Cryptanalyst(string testDataName) : base(testDataName)
         {
-            this.ExcludeTestCaseRangeInclusive(1, 2);
-            this.ExcludeTestCaseRangeInclusive(11, 37);
-            //this.ExcludeTestCaseRangeInclusive(27, 37);
+            this.ExcludeTestCaseRangeInclusive(31, 37);
             LoadDictionary();
         }
 
@@ -41,20 +39,42 @@ namespace Exam1
 
             
             string result = null;
-            int range = 10;
-            for(int i = 0; i<range; i++)
+            string pass = GetFirstPass();
+            while(true)
             {
-                Encryption encryption = new Encryption(i.ToString(), ' ', 'z', false);
+                Encryption encryption = new Encryption(pass, ' ', 'z', false);
                 result = encryption.Decrypt(cipher);
                 List<string> words = result.Split(' ').ToList();
                 if (VerifyResult(words))
                     break;
+                
+                pass = MakePass(pass);
             }
 
             TestCaseNumber++;
             return result.GetHashCode().ToString();
         }
-        
+
+        private string GetFirstPass()
+        {
+            int digits = TestCaseNumber / 10 + 1;
+            string result = "0";
+            while (result.Length < digits)
+                result = "0" + result;
+
+            return result;
+        }
+
+        private string MakePass(string prevNumber)
+        {
+            int digits = TestCaseNumber / 10 + 1;
+            int newNumber = int.Parse(prevNumber) + 1;
+            string result = newNumber.ToString();
+            while (result.Length < digits)
+                result = "0" + result;
+
+            return result;
+        }
 
         private bool VerifyResult(List<string> words)
         {
@@ -68,7 +88,7 @@ namespace Exam1
 
             int precentage = (int)(containCount * 100 / wordCount);
 
-            return  precentage >= 80;
+            return  precentage >= 75;
         }
     }
 }
