bda5362d21eb1b4057ec11fcf538e0eb7b3dd29c
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-05-23 18:15:20 +0430

    Update Q1 Testcases Add Q1 Solution

---
+++ A9/A9/Equation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A9
+{
+    public class Equation
+    {
+        public double[,] Coefficients { get; set; }
+        public double[] Intercepts { get; set; }
+        public int Size { get; set; }
+
+        public Equation(long size, double[,] matrix)
+        {
+            Coefficients = new double[size,size];
+            Intercepts = new double[size];
+            Size = (int)size;
+
+            for(int i=0; i<size; i++)
+            {
+                Intercepts[i] = matrix[i, size];
+
+                for (int j = 0; j < size; j++)
+                    Coefficients[i, j] = matrix[i, j];
+            }
+        }
+    }
+
+    public class Pivot
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Pivot(int row, int col)
+        {
+            Row = row;
+            Column = col;
+        }
+    }
+}
