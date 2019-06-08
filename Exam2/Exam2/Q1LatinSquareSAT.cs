479913ceae6b76598c3a53bdfa08ea6b3472f3b4
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-06-08 17:49:30 +0430

    Add Q1 Solution

--- Exam2/Exam2/Q1LatinSquareSAT.cs
+++ Exam2/Exam2/Q1LatinSquareSAT.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TestCommon;
+using Combinatorics.Collections;
 
 namespace Exam2
 {
     public class Q1LatinSquareSAT : Processor
     {
         public Q1LatinSquareSAT(string testDataName) : base(testDataName)
-        {}
+        {
+            //this.ExcludeTestCaseRangeInclusive(41, 54);
+        }
 
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<int,int?[,],string>)Solve);
@@ -20,30 +23,98 @@ namespace Exam2
 
         public string Solve(int dim, int?[,] square)
         {
-            GetClauses(dim, square);
-            throw new NotImplementedException();
-        }
-
-        private void GetClauses(int dim, int?[,] square)
-        {
-            List<string> clauses = new List<string>() { "0 0" };
-            List<int> variables = new List<int>();
-            for (int k = 0; k < dim; k++)
-                variables.Add(k);
-
-            CellConstraint(dim, square, ref clauses);
-            throw new NotImplementedException();
-        }
-
-        private void CellConstraint(int dim, int?[,] square, ref List<string> clauses)
-        {
+            StringBuilder builder = new StringBuilder();
+            int count = 0;
+            count+=CellConstraint(dim, ref builder);
+            count+=RowConstraint(dim, ref builder);
+            count+=ColumnConstraint(dim, ref builder);
             
+            return ConstructFormula(dim, square, ref builder, ref count);
+        }
+
+        private string ConstructFormula(int dim, int?[,] square,
+                        ref StringBuilder builder,ref int count)
+        {
             for(int i=0; i<dim; i++)
                 for(int j=0; j<dim; j++)
                 {
-
+                    if ( square[i,j].HasValue)
+                    {
+                        count++;
+                        builder.Append(Varnum(i, j, square[i, j].Value, dim).ToString() + "\n");
+                    }
+                        
                 }
-            throw new NotImplementedException();
+            return Math.Pow(dim, 3).ToString() + " " + count.ToString() + "\n" + builder.ToString();
         }
+        
+
+        private int ColumnConstraint(int dim, ref StringBuilder builder)
+        {
+            int count = 0;
+            for(int j=0; j<dim; j++)
+                for(int k=0; k<dim; k++)
+                {
+                    List<int> literals = new List<int>();
+                    for (int i = 0; i < dim; i++)
+                        literals.Add(Varnum(i, j, k, dim));
+
+                    count+=ExactlyOneOf(literals, ref builder);
+                }
+            return count;
+        }
+
+        private int RowConstraint(int dim, ref StringBuilder builder)
+        {
+            int count = 0;
+            for(int i=0; i<dim; i++)
+                for(int k=0; k<dim; k++)
+                {
+                    List<int> literals = new List<int>();
+                    for (int j = 0; j < dim; j++)
+                        literals.Add(Varnum(i, j, k, dim));
+
+                    count+=ExactlyOneOf(literals, ref builder);
+                }
+            return count;
+        }
+
+        private int CellConstraint(int dim, ref StringBuilder builder)
+        {
+            int count = 0;
+            for(int i=0; i<dim; i++)
+                for(int j=0; j<dim; j++)
+                {
+                    List<int> literals = new List<int>();
+
+                    for (int k = 0; k < dim; k++)
+                        literals.Add(Varnum(i, j, k, dim));
+
+                    count+=ExactlyOneOf(literals, ref builder);
+                        
+                }
+            return count;
+        }
+
+        private int ExactlyOneOf(List<int> literals, ref StringBuilder builder)
+        {
+            int count = 1;
+            builder.Append(literals.Select(x => x.ToString())
+                    .Aggregate((s1, s2) => s1 + " " + s2).ToString() + "\n");
+
+            Combinations<int> pairs = new Combinations<int>(literals, 2);
+            
+            foreach (List<int> pair in pairs)
+            {
+                builder.Append(pair.Select(x => (-x).ToString())
+                    .Aggregate((s1, s2) => s1 + " " + s2 + "\n").ToString()
+                   );
+                count++;
+            }
+
+            return count;
+        }
+
+        private int Varnum(int i, int j, int k, int dim) => (i * (dim * dim)) + (j * dim) + k + 1;
     }
 }
